4016ab23c8152c977951aa608194efd9fa79e919
Ali Uraish <aliuraishmirani@gmail.com>
2025-11-19 01:00:54 +0500

    Solved Part A and B. Part C and D coming up here.

---
+++ ON-2022-42-Q1/Part_A_B_C_D.py
@@ -0,0 +1,8 @@
+#PartA
+# Declare a global string array "Animals" to store 10 items
+Animals: list[str]=[]
+
+#PartB
+#Store the given names in the Animals array
+#They should be stored in the order given and in lower case
+Animals=["horse", "lion", "rabbit", "mouse", "bird", "deer", "whale", "elephant", "kangaroo", "tiger"]
