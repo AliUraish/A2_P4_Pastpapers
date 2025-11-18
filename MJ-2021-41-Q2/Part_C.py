f37302e4ca04e25b4f3b0869fc8289e5092b893f
Ali Uraish <aliuraishmirani@gmail.com>
2025-11-19 00:26:59 +0500

    Solved the last part, part C

--- MJ-2021-41-Q2/Part_C.py
+++ MJ-2021-41-Q2/Part_C.py
@@ -1,2 +1,14 @@
 # Write a bubblesort() algorithm to sort the data in the "theArray" into descending order.
 # The pseudocode is already given in the question paper.
+theArray=[5, 6, 2, 8, 1, 4, 9, 3, 7, 0]
+def bubblesort():
+    for x in range(len(theArray)-1):
+        for y in range(len(theArray)-1):
+            if theArray[y]<theArray[y+1]:
+                temp=theArray[y]
+                theArray[y]=theArray[y+1]
+                theArray[y+1]=temp
+                
+#this test was not included in the question, you can run the code and test it yourself for your assurance. 
+bubblesort()
+print("Sorted array in descending order:", theArray)
