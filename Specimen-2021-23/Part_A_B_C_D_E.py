6b49de3b30eb41a0247d4a08b8d69a32d56ba492
Ali Uraish <aliuraishmirani@gmail.com>
2025-11-19 19:02:33 +0500

    Part E(ii)

--- Specimen-2021-23/Part_A_B_C_D_E.py
+++ Specimen-2021-23/Part_A_B_C_D_E.py
@@ -6,10 +6,10 @@ TheData=[20, 3, 4, 8, 12, 99, 4, 26, 4]
 #Pseudocode for an insertion sort algorithm to sort the items in TheData array into ascending order
 def InsertionSort(TheData):
     Inserted=0
-    for count in range(1,len(TheData)):
+    for count in range(1, len(TheData)):
         Datatoinsert=TheData[count]
         NextValue=count-1
-        while NextValue>=0 and count!=1:
+        while NextValue>=0 and TheData[NextValue]>Datatoinsert:
             if Datatoinsert<TheData[NextValue]:
                 TheData[NextValue+1]=TheData[NextValue]
                 NextValue=NextValue-1
@@ -30,8 +30,7 @@ def output(Array):
 
 #PartD (i)
 #Show data in TheData array before sorting and after sorting using subroutines in part B and C
-unsorted=output(TheData)
-print(unsorted)
+output(TheData)
 sorted=InsertionSort(TheData)
 print(sorted)
 
@@ -41,12 +40,15 @@ print(sorted)
 #PartE(i)
 #Make a searching program for array TheData in a parameter value to search through the array
 def search(value):
-    global MyData
     for index in range(len(TheData)):
         if TheData[index]==value:
             print("Found")
+            break
     else:
         print("Not Found")
 
+
 #PartE(ii)
 #Test the searching program with 8 and 9
+search(8)
+search(9)
