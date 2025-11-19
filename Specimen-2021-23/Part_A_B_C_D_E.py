0f9de7505671d3e54640e2c1a312be6439e79356
Ali Uraish <aliuraishmirani@gmail.com>
2025-11-19 17:11:39 +0500

    Part C: Implement insertion sort algorithm for sorting TheData array

--- Specimen-2021-23/Part_A_B_C_D_E.py
+++ Specimen-2021-23/Part_A_B_C_D_E.py
@@ -3,3 +3,21 @@
 TheData=[20, 3, 4, 8, 12, 99, 4, 26, 4]
 
 #PartB
+#Pseudocode for an insertion sort algorithm to sort the items in TheData array into ascending order
+def InsertionSort():
+    global TheData
+    Inserted=0
+    for count in range(1,len(TheData)):
+        Datatoinsert=TheData[count]
+        NextValue=count-1
+        while NextValue>=0 and count!=1:
+            if Datatoinsert<TheData[NextValue]:
+                TheData[NextValue+1]=TheData[NextValue]
+                NextValue=NextValue-1
+                TheData[NextValue+1]=Datatoinsert
+            else:
+                Inserted=1
+
+
+#PartC
+
