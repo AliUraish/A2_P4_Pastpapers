f72bd35998d83fc2372dc83ee5f44613a8f64c2e
Ali Uraish <aliuraishmirani@gmail.com>
2026-03-24 22:46:11 +0500

    part b

--- MJ-2022-42-Q2/Part_A.py
+++ MJ-2022-42-Q2/Part_A.py
@@ -3,3 +3,14 @@ arraydata=[[0]*10 for i in range(10)]
 for i in range(10):
     for j in range(10):
         arraydata[i][j]=random.randint(0,100)
+
+#part b
+arraylength=10
+for x in range(0, arraylength):
+    for y in range(0, arraylength-1):
+        for z in range(0, arraylength - y - 1):
+            if arraydata[x][z] > arraydata[x][z+1]:
+                tempnumber=arraydata[x][z]
+                arraydata[x][z]=arraydata[x][z+1]
+                arraydata[x][z+1]=tempnumber
+
