1213b9e5ab349825791f5b4f53591909f04a064d
Ali Uraish <aliuraishmirani@gmail.com>
2026-04-07 14:18:29 +0500

    Question done and tested

--- MJ2023/41-Q1/Parta.py
+++ MJ2023/41-Q1/Parta.py
@@ -8,3 +8,27 @@ try:
 except FileNotFoundError:
     print("File not Found")
 
+def printarray(DataArray):
+    output=""
+    for i in range(0, len(DataArray)):
+        output=output+str(DataArray[i])+" "
+    print(output)
+
+printarray(DataArray)
+
+
+def linear_search(DataArray, value):
+    count=0
+    for i in range(0, len(DataArray)):
+        if DataArray[i]==value:
+            count=count+1
+    return count
+
+
+inputvalue=int(input("Enter a value to search for between 0 and 100: "))
+while inputvalue<0 or inputvalue>100:
+    print("invalid input")
+    inputvalue=int(input("Enter a value to search for between 0 and 100: "))
+
+Number_times=linear_search(DataArray, inputvalue)
+print("The number,",inputvalue,"is found",Number_times,"times")
