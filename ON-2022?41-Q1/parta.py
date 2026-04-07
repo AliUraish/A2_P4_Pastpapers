a09347087f76eb6e8ebd6595ed670b76b2173f06
Ali Uraish <aliuraishmirani@gmail.com>
2026-04-07 23:03:52 +0500

    till part d done

--- ON-2022?41-Q1/parta.py
+++ ON-2022?41-Q1/parta.py
@@ -9,3 +9,19 @@ def ReadFile():
         DataFile.close()
     except FileNotFoundError:
         print("File not found")
+
+def FindValue():
+    global DataArray
+    inputvalue=int(input("Enter a value to search"))
+    while inputvalue<0 or inputvalue>100:
+        print("invalid input")
+        inputvalue=int(input("Enter a value to search between 0 and 100: "))
+    Number_times=0
+    for i in range(0, len(DataArray)):
+        if DataArray[i]==inputvalue:
+            Number_times+=1
+    return Number_times
+
+ReadFile()
+print("The number appears",FindValue(),"times")
+
